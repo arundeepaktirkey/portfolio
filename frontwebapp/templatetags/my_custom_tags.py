51e1edcdea1edb4e061f8d9fb450df907d07033a
arundeepaktirkey <arundeepak86999@gmail.com>
2024-05-17 07:10:20 +0100

    added custom tags and few changes in views.py

--- frontwebapp/templatetags/my_custom_tags.py
+++ frontwebapp/templatetags/my_custom_tags.py
@@ -3,10 +3,12 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+inverted = False
+
 @register.simple_tag
 def render_experience(workexps):
+    global inverted
     output = ''
-    inverted = False
     for experience in workexps:
         li_class = 'timeline-inverted animate-box' if inverted else 'animate-box timeline-unverted'
         inverted = not inverted
@@ -20,7 +22,32 @@ def render_experience(workexps):
                     </div>
                     <div class="timeline-body">
                         <p>{experience. description}</p>
-                        <p><b><u>Skills</u> - </b>{', '.join(experience.skills)}</p>
+                        <p><b><u>Skills</u> - </b>{experience.skills}</p>
+                    </div>
+                </div>
+            </li>
+        '''
+
+    return mark_safe(output)
+
+@register.simple_tag
+def render_education(educations):
+    output = ''
+    global inverted
+    for education in educations:
+        li_class = 'timeline-inverted animate-box' if inverted else 'animate-box timeline-unverted'
+        inverted = not inverted
+        output += f'''
+            <li class="{li_class}">
+                <div class="timeline-badge"><i class="icon-suitcase"></i></div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h3 class="timeline-title">{education.degree}</h3>
+                        <span class="company">{education.university}, {education.location}  ({education.start_date} - {education.end_date})</span>
+                    </div>
+                    <div class="timeline-body">
+                        <p>{education. description}</p>
+                        <p><b><u>Skills</u> - </b>{education.skills}</p>
                     </div>
                 </div>
             </li>
--- frontwebapp/views.py
+++ frontwebapp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from .models import WorkExp
+from .models import WorkExp, Education
 # Create your views here.
 def home(request):
     workex = WorkExp.objects.all()
+    educations = Education.objects.all()
+
     # get_count = WorkExp.objects.all().count() 
     context = {
         'workexps': workex,
+        'educations' : educations
         }
     return render(request, 'frontwebapp/home.html', context = context)
