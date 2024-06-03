6239b06e214ca7fd5d25e5810a441163016157ea
arundeepaktirkey <arundeepak86999@gmail.com>
2024-06-03 13:47:56 +0100

    Added contact forms

--- frontwebapp/models.py
+++ frontwebapp/models.py
@@ -71,4 +71,15 @@ class Description(models.Model):
     def __str__(self):
         return self.text[:50]
 
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
     
--- frontwebapp/views.py
+++ frontwebapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from .models import WorkExp, Education, MyServices, Internships, Projects
+from .forms import contactsforms
 # Create your views here.
 def home(request):
     workex = WorkExp.objects.all()
@@ -8,12 +10,22 @@ def home(request):
     internships = Internships.objects.all()
     projects = Projects.objects.all()
 
-    
+    if request.method == 'POST':
+        form = contactsforms(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('frontwebapp/home.html')  # Replace 'success_url' with the desired URL name
+    else:
+        form = contactsforms()
+
     context = {
         'workexps': workex,
         'educations' : educations,
         'services' : services,
         'internships': internships,
         'projects': projects,
+        'form': form
         }
     return render(request, 'frontwebapp/home.html', context = context)
+
+
